a745cd94b38cccb6decd0a2a06c658d9e5953989
Titor John <huzra0302@outlook.com>
2018-10-30 21:06:49 +0800

    aha

---
+++ Jmeter TEST/src/com/opensource/jmeter/com_opensource_jmeter.java
@@ -0,0 +1,23 @@
+package com.opensource.jmeter;
+
+public class com_opensource_jmeter {
+	private String name;
+	private String friend;
+	public void setName(String name) 
+	{
+		this.name=name;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setFriend(String friend)
+	{
+		this.friend=friend;
+	}
+	public String getFriend()
+	{
+		return friend;
+	}
+}
